0da998333d10b5a8f49f84e9b4227fabb67fd8c9
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-03-28 13:39:07 +0000

    merged leaderboard with login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import "./App.css";
+import './App.css'
 import Leaderboard from "./Leaderboard/Leaderboard";
+import login from './login'
 
 function App() {
+  
+
   return (
     <>
-      <div>
-        <Leaderboard />
+      <div className="container">
+      <Leaderboard />
+        <div className="column">
+        <h1>Welcome to SheCodes!</h1>
+        </div>
+        <div className="login-card">
+          <div className="column">
+        <Login />
+        </div>
       </div>
+      </div>
+
     </>
-  );
+  )
 }
 
 export default App;
--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,20 +1,40 @@
+import React from "react";
+import { useState } from "react";
 
 function Login() {
+    const [email, setEmail] = useState("");
+
+    const handleEmailSubmit = (event) => {
+        setEmail(event.value)
+    }
   
 
     return (
       <>
-        <div>
-        </div>
+      <form onSubmit={handleEmailSubmit}>
         <div className="login-card">
-            <form>
-            <label>
-            <input> type="text" name="email"</input>
-            </label>
-            </form>
+            <div>
+            <input
+             id="email"
+             type="email"
+             placeholder="School Email Address"
+             className="form-login"
+             value={email}
+            />
+            </div>
+            <button
+            
+            role="submit"
+            className="submit-button"
+            id="submit"
+            type="submit"
+            />
+
         </div>
+        </form>
       </>
+      
     )
   }
   
-  export default Login();
+  export default Login;
