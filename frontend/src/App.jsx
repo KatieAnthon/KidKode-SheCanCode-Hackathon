38decbbc162d316eef4f252250c292ff0d62a6c0
Estelle-Lee <bokyunglee33@gmail.com>
2024-03-28 16:01:36 +0000

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './Profile/SchoolProfile'
+import SchoolProfile from './Profile/SchoolProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <SchoolProfile/>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
