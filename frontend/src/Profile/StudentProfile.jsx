0e52be94bac45c249dc26fb2503ef7bfd2728a30
Hazal Deniz Kara <94861070+hazalDK@users.noreply.github.com>
2024-03-28 13:42:39 +0000

    added some styling

--- frontend/src/Profile/StudentProfile.jsx
+++ frontend/src/Profile/StudentProfile.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
+import "./StudentProfile.css"
 
 export default function StudentProfile() {
-    const isLoggedIn = useState(true)
     const userName = useState("placeholder")
     const points = useState(3333)
     const name = useState("placeholder name")
     return (
-        <div>
-            <p>UserName: </p> {userName}
-            <p>Name: </p> {name}
-            <p>Points: </p> {points}
-            <p>Badges: </p> 
-
+        <div className="StudentProfile">
+            <div className="studentCard">
+                <img src="img.jpg" alt="John" style={{ width: "100%" }} />
+                <h1>{userName}</h1>
+                <h3>{name}</h3>
+                <p>Active Since: </p>
+                <p>Challenges won: </p>
+            </div>
+            <div className="studentRank">
+                <div className="points">
+                    <p>Your Points: </p>
+                    {points}
+                </div>
+                <div className="badges">
+                    <p>Badges: </p> 
+                </div>
+            </div>
+            <button>Start a challenge</button>
         </div>
     )
 }
